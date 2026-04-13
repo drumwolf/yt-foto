57b7ef05a1aa84f725282b4e2740a9c17e8b9574
John Lee <macmini@Johns-Mac-mini-2.local>
2026-04-12 18:19:50 -0700

    first draft of basic youtube ID thumbnail form

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,44 @@ import { useState } from 'react'
 
 function App() {
   const [videoID, setVideoID] = useState('')
+  const [videoData, setVideoData] = useState<any | null>(null)
+
+  async function fetchVideoData() {
+    const apiKey = import.meta.env.VITE_YOUTUBE_API_KEY
+    const res = await fetch(
+      `https://www.googleapis.com/youtube/v3/videos?id=${videoID}&part=snippet&key=${apiKey}`
+    )
+    const data = await res.json()
+    const item = data.items?.[0]
+    if (item) {
+      setVideoData(item.snippet)
+    }
+  }
 
   return (
     <section className="p-4 text-center">
       <h1 className="mb-2">yt-foto</h1>
-      <div>
+      <div className="mb-5">
         <Input
           className="w-[300px]"
           onChange={(e: any) => setVideoID(e.target.value)}
         />
         <Button
           className="my-3 px-4 ml-2"
-          onClick={() => console.log(videoID)}
+          onClick={fetchVideoData}
         >
-          Submit YouTube URL
+          Fetch video data
         </Button>
       </div>
       {videoID && (
-        <img className="mx-auto" src={`https://img.youtube.com/vi/${videoID}/maxresdefault.jpg`} />
+        <div>
+          <img
+            className="mx-auto w-[600px] my-3"
+            src={`https://img.youtube.com/vi/${videoID}/maxresdefault.jpg`}
+          />
+          <p><strong>Filename:</strong>{}</p>
+          <p>{`(${videoData?.publishedAt.slice(0,10)}) ${videoID}`}</p>
+        </div>
       )}
     </section>
   )
