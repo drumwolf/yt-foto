1cab53ffec617a5048ea9c5b0611db00e6703a65
John Lee <macmini@Johns-Mac-mini-2.local>
2026-04-12 17:49:58 -0700

    first draft of input and button interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
 import './App.css'
 
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { useState } from 'react'
+
 function App() {
+  const [videoID, setVideoID] = useState('')
+
   return (
     <section className="p-4 text-center">
-      <h1>yt-foto</h1>
+      <h1 className="mb-2">yt-foto</h1>
+      <div>
+        <Input
+          className="w-[300px]"
+          onChange={(e: any) => setVideoID(e.target.value)}
+        />
+        <Button
+          className="my-3 px-4 ml-2"
+          onClick={() => console.log(videoID)}
+        >
+          Submit YouTube URL
+        </Button>
+      </div>
     </section>
   )
 }
